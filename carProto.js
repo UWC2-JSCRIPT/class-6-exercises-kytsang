e98897be1f21c724f76fda6ff2875a9d6cbf6b35
kytsang <kytsang@uw.edu>
2022-11-29 16:15:42 -0800

    Homework Submit Attempt1 - Nov29

--- carProto.js
+++ carProto.js
@@ -4,7 +4,31 @@
  * @param {String} model
  */
 
+
+class Car{
+    constructor(model, currentSpeed, accelerateUnits, brakeUnits) {
+         this.model = model;
+         this.currentSpeed=currentSpeed;
+         this.accelerateUnits=accelerateUnits;
+         this.brakeUnits=brakeUnits;
+    }
+
+    toString= function(){
+       let newSpeed= this.currentSpeed +(1* this.accelerateUnits)-(1* this.brakeUnits);
+        
+        console.log(`Car model ${this.model} is now at speed of ${newSpeed}`);
+    }
+}
+
+
+
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const tacoma = new Car('tacoma', 0, 2, 1);
+tacoma.toString();
+
+
+
 
 /**
  * ElectricCar class
@@ -12,4 +36,21 @@
  * @param {String} model
  */
 
+ class ElectricCar extends Car {
+    constructor(model, currentSpeed, accelerateUnits, brakeUnits, motor){
+        super(model, currentSpeed, accelerateUnits, brakeUnits);
+        this.motor=motor;
+    }
+    toString= function(){
+        let newSpeed= this.currentSpeed +(1* this.accelerateUnits*2)-(1* this.brakeUnits);
+         
+         console.log(`Car model ${this.model} is now at speed of ${newSpeed}`);
+     }
+}
+
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const tesla = new ElectricCar('tesla', 0, 2, 1, 'electric');
+tesla.toString();
+
+
--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,10 +1,27 @@
+$(document).ready(function(){
+
 /**
  * Toggles "done" class on <li> element
  */
 
+
+const $list = $('li');
+
+$list.on('click', function(){
+  $list.className='done';
+})
+
+
 /**
  * Delete element when delete link clicked
  */
+
+const $del = $('.delete');
+
+$del.on('click', function(){
+  $list.remove();
+})
+});
 
 /**
  * Adds new list item to <ul>
