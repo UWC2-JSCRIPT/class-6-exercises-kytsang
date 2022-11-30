5321f8dab3b2c9483d593efe2d8e165ebb5f5f8c
kytsang <kytsang@uw.edu>
2022-11-29 22:25:21 -0800

    Homework Nov29-10:30pm attempt

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
 /**
  * Toggles "done" class on <li> element
  */
-
-
 const $list = $('li');
 
 $list.on('click', function(){
-  $list.className='done';
+   this.className='done';
 })
 
 
@@ -16,21 +14,60 @@ $list.on('click', function(){
  * Delete element when delete link clicked
  */
 
-const $del = $('.delete');
+  const $del = $('.delete');
+  
+  $del.on('click', function(e){
+      $(e.target).parent().fadeOut("slow");
+  })
 
-$del.on('click', function(){
-  $list.remove();
-})
 });
 
 /**
  * Adds new list item to <ul>
  */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+// const addListItem = function(e) {
+//   e.preventDefault();
+//   const text = $('input').val();
 
-  // rest here...
+//   // rest here...
+// };
+
+$(document).ready(function(){
+
+const addListItem = function(button, e) {
+  e.preventDefault();
+  const input = button.parentNode.getElementsByTagName('input')[0];  
+  const text = $('input').val(); // use this text to create a new <li>
+  const aElement = document.createElement('li');
+  const childSpan = document.createElement('span');
+  aElement.appendChild(childSpan);
+  childSpan.innerText=text;
+  const todayList = document.getElementsByClassName("today-list")[0];
+  todayList.appendChild(aElement);
+  const deleteButton =document.createElement('a');
+  aElement.appendChild(deleteButton);
+  deleteButton.innerText= 'Delete';
+  deleteButton.className='delete';
+
+  aElement.addEventListener('click', ()=> {
+    aElement.className='done';
+  })
+
+  deleteButton.addEventListener('click', ()=> {
+    aElement.remove();
+  })
+
 };
 
+
 // add listener for add
+
+  const $add = $('.add-item:first');   
+
+  $add.on('click', function(e){
+    addListItem(this,e);
+})
+
+});
+
+
